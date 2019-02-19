9507b5180187b08c1b875f36f9e4caa8596a89a3
Vivek.Thakkar <Vivek.Thakkar@DESKTOP-8HIMRDR>
2019-02-19 00:14:53 -0500

    Initial Commit

---
+++ logTest/src/main/java/logTest/logBackTest.java
@@ -0,0 +1,21 @@
+package logTest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+public class logBackTest {
+
+
+	public static void main(String[] args) {
+		Logger logger=LoggerFactory.getLogger(logBackTest.class);
+		logger.debug("This is debug message");
+		logger.info("log message from info" +logBackTest.class.hashCode());
+		
+		
+		logger.debug("Hello this is a debug message");
+		logger.info("Hello this is an info message");
+	  	   	
+	}
+}
+
